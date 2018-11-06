e88a1c40fe1cd5249c6b0b3421ad7e8ef683c151
shelisiboni <shelisiboni111@gmail.com>
2018-11-06 19:30:53 +0200

    change weather from kelvin to celcius

--- BooksReview/BooksReview/Scripts/weather.js
+++ BooksReview/BooksReview/Scripts/weather.js
@@ -1,10 +1,9 @@
-﻿var apiCalls = 'http://api.openweathermap.org/data/2.5/weather?q=Tel' + " " + 'Aviv&appid=bdb3f76b0cb6e16136aec2410502dd16'
+﻿var apiCalls = 'http://api.openweathermap.org/data/2.5/weather?id=293397&units=metric' + '&appid=bdb3f76b0cb6e16136aec2410502dd16'
 $.getJSON(apiCalls, weatherCallBack)
 
 function weatherCallBack(WeatherData) {
     var temp = WeatherData.main.temp;
-    temp -= 273.15;
     var city = WeatherData.name;
 
-    document.getElementById('tempP').append(" " + temp + "C degrees.");
+    document.getElementById('tempP').append("  " + temp + " °C");
 };
